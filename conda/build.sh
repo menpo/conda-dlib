a95cbd3af0d4097178d9045d30e04f6974cd0efc
Patrick Snape <patricksnape@gmail.com>
2014-10-31 12:23:25 +0000

    Add OSX deployment target

--- conda/build.sh
+++ conda/build.sh
@@ -2,16 +2,17 @@
 mkdir build
 cd build
 
-CMAKE_GENERATOR="Unix Makefiles"
 CMAKE_ARCH="-m"$ARCH
 
 if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   export CFLAGS="$CFLAGS -fPIC $CMAKE_ARCH"
   export LDFLAGS="$LDFLAGS $CMAKE_ARCH"
   DYNAMIC_EXT="so"
+  EXTRA_FLAGS=""
 fi
 if [ "$(uname -s)" == "Darwin" ]; then
   DYNAMIC_EXT="dylib"
+  EXTRA_FLAGS="-DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET}"
 fi
 
 if [ $PY3K -eq 1 ]; then
@@ -20,20 +21,20 @@ else
   export PY_STR="${PY_VER}"
 fi
 
-export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
+export LDFLAGS="-L${LIBRARY_PATH} $LDFLAGS"
 
-cmake ../tools/python -G"$CMAKE_GENERATOR" \
+cmake ../tools/python \
 -DCMAKE_PREFIX_PATH=$PREFIX \
 -DBUILD_SHARED_LIBS=1 \
 -DBoost_USE_STATIC_LIBS=0 \
 -DBoost_USE_STATIC_RUNTIME=0 \
--DBOOST_INCLUDEDIR=$PREFIX/include \
--DBOOST_LIBRARYDIR=$PREFIX/lib \
--DPYTHON_LIBRARY=$PREFIX/lib/libpython$PY_STR.$DYNAMIC_EXT \
--DPYTHON_INCLUDE_DIR=$PREFIX/include/python$PY_STR \
+-DBOOST_INCLUDEDIR="${INCLUDE_PATH}" \
+-DBOOST_LIBRARYDIR="${LIBRARY_PATH}" \
+-DPYTHON_LIBRARY="${LIBRARY_PATH}/libpython$PY_STR.$DYNAMIC_EXT" \
+-DPYTHON_INCLUDE_DIR="${INCLUDE_PATH}/python$PY_STR" \
 -DDLIB_NO_GUI_SUPPORT=1 \
 -DDLIB_USE_BLAS=0 \
--DDLIB_USE_LAPACK=0
+-DDLIB_USE_LAPACK=0 ${EXTRA_FLAGS}
 # Above should be changed when the MKL features problem is solved
 
 cmake --build . --config Release --target install
