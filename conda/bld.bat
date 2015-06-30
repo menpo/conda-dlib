3b5ebbf7d842228a4ae1b254b4c8f5017694522b
Patrick Snape <patricksnape@gmail.com>
2015-06-30 10:21:14 +0100

    Allow Python3 building
    Update CMAKE to take into account using Visual Studio 2010 for Py34
    builds.

--- conda/bld.bat
+++ conda/bld.bat
@@ -5,15 +5,19 @@ cd build
 
 rem Need to handle Python 3.x case at some point (Visual Studio 2010)
 if %ARCH%==32 (
-  if %PY_VER% LSS 3 (
     set CMAKE_CONFIG="Release|Win32"
+  if %PY_VER% LSS 3 (
 	set GENERATOR="Visual Studio 9 2008"
+  ) else (
+	set GENERATOR="Visual Studio 10"
   )
 )
 if %ARCH%==64 (
-  if %PY_VER% LSS 3 (
     set CMAKE_CONFIG="Release|x64"
+  if %PY_VER% LSS 3 (
 	set GENERATOR="Visual Studio 9 2008 Win64"
+  ) else (
+	set GENERATOR="Visual Studio 10 Win64"
   )
 )
 
