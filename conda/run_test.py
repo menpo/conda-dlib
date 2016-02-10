ec5276266dc8019dcfcc973d2fe36c02f234a9ea
Patrick Snape <patricksnape@gmail.com>
2016-02-10 13:31:42 +0000

    Add missing lena.png from scipy
    scipy removed lena.png - so we add it explicitly here. Also,
    downgrade to older boost

--- conda/run_test.py
+++ conda/run_test.py
@@ -1,14 +1,21 @@
 import sys
+import os
 import dlib
 import numpy as np
-from scipy import misc
+from PIL import Image
 
 if __name__ == "__main__":
     try:
-        lena = misc.lena().astype(np.uint8)
+        recipe_filepath = os.environ.get('RECIPE_DIR', os.path.dirname(os.path.abspath(__file__)))
+        lena_path = os.path.join(recipe_filepath, 'lena.png')
+        
+        lena = np.array(Image.open(lena_path))
         detector = dlib.get_frontal_face_detector()
         results = detector(lena)
+        
         print('Found {} faces'.format(len(results)))
         assert(len(results) == 1)
-    except:
+    except Exception as e:
+        print(e)
         sys.exit(1)
+
