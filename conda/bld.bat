dd270a0563e6c7e373714e7df6f914600a6e1e9d
Patrick Snape <patricksnape@gmail.com>
2016-05-23 17:18:43 +0100

    Force release mode on windows

--- conda/bld.bat
+++ conda/bld.bat
@@ -4,13 +4,12 @@ setlocal EnableDelayedExpansion
 mkdir build
 cd build
 
-set CMAKE_CONFIG="Release"
-
 rem The Python lib has no period in the
 rem version string, so we remove it here.
 set PY_VER_NO_DOT=%PY_VER:.=%
 
 cmake ..\tools\python -LAH -G"NMake Makefiles" ^
+-DCMAKE_BUILD_TYPE="Release" ^
 -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
 -DBUILD_SHARED_LIBS=1 ^
 -DBoost_USE_STATIC_LIBS=0 ^
@@ -29,8 +28,7 @@ cmake ..\tools\python -LAH -G"NMake Makefiles" ^
 -DDLIB_USE_LAPACK=0 ^
 -DUSE_SSE4_INSTRUCTIONS=0
 
-cmake --build . --config %CMAKE_CONFIG% --target ALL_BUILD
-cmake --build . --config %CMAKE_CONFIG% --target INSTALL
+cmake --build . --config Release --target INSTALL
 
 rem Copy the dlib library to site packages
 move "..\python_examples\dlib.pyd" "%SP_DIR%\dlib.pyd"
