9de80eaa27416c8ca237ca698cc9823bf1b341de
Patrick Snape <patricksnape@gmail.com>
2017-05-08 00:12:47 +0200

    Try adding Python 3.6/upgrade to 19.4

--- conda/build.sh
+++ conda/build.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
+
+rm -fr build
 mkdir build
 cd build
 
-CMAKE_ARCH="-m"$ARCH
-INCLUDE_PATH=${PREFIX}/include
-LIBRARY_PATH=${PREFIX}/lib
-
 if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-  export CFLAGS="$CFLAGS -fPIC $CMAKE_ARCH"
-  export LDFLAGS="$LDFLAGS $CMAKE_ARCH"
+  export CXXFLAGS="${CXXFLAGS} -DBOOST_MATH_DISABLE_FLOAT128"
   DYNAMIC_EXT="so"
-  EXTRA_FLAGS=""
-  DLIB_JPEG="-DJPEG_INCLUDE_DIR=${INCLUDE_PATH} -DJPEG_LIBRARY=${LIBRARY_PATH}/libjpeg.${DYNAMIC_EXT}"
 fi
 if [ "$(uname -s)" == "Darwin" ]; then
-  DLIB_JPEG=""
+export MACOSX_VERSION_MIN="10.9"
+  export MACOSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}"
+  export CMAKE_OSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}"
+  export CFLAGS="${CFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+  export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+  export CXXFLAGS="${CXXFLAGS} -std=c++11 -stdlib=libc++"
+  export LDFLAGS="${LDFLAGS} -headerpad_max_install_names"
+  export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+  export LDFLAGS="${LDFLAGS} -lc++"
   DYNAMIC_EXT="dylib"
-  EXTRA_FLAGS="-DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET}"
 fi
 
 if [ $PY3K -eq 1 ]; then
@@ -34,39 +36,38 @@ else
   USE_SSE4=0
 fi
 
-export LDFLAGS="-L${LIBRARY_PATH} $LDFLAGS"
+PYTHON_LIBRARY_PATH="$PREFIX/lib/libpython$PY_STR.$DYNAMIC_EXT"
 
-cmake -LAH ../tools/python \
--DCMAKE_PREFIX_PATH=$PREFIX \
--DBUILD_SHARED_LIBS=1 \
--DBoost_USE_STATIC_LIBS=0 \
--DBoost_USE_STATIC_RUNTIME=0 \
--DBOOST_ROOT=$PREFIX \
--DBOOST_INCLUDEDIR="${INCLUDE_PATH}" \
--DBOOST_LIBRARYDIR="${LIBRARY_PATH}" \
--DPYTHON_LIBRARY="${LIBRARY_PATH}/libpython$PY_STR.$DYNAMIC_EXT" \
--DPYTHON_INCLUDE_DIR="${INCLUDE_PATH}/python$PY_STR" \
--DPYTHON3=$PY3K \
--DPNG_INCLUDE_DIR="${INCLUDE_PATH}" \
--DPNG_PNG_INCLUDE_DIR="${INCLUDE_PATH}" \
--DPNG_LIBRARY="${LIBRARY_PATH}/libpng.${DYNAMIC_EXT}" \
--DZLIB_INCLUDE_DIRS="${INCLUDE_PATH}" \
--DZLIB_LIBRARIES="${LIBRARY_PATH}/libz.${DYNAMIC_EXT}" \
-${DLIB_JPEG} \
--DDLIB_LINK_WITH_SQLITE3=1 \
--DDLIB_NO_GUI_SUPPORT=1 \
--DUSE_SSE2_INSTRUCTIONS=1 \
--DUSE_SSE4_INSTRUCTIONS=$USE_SSE4 \
--DDLIB_USE_BLAS=1 \
--DDLIB_USE_LAPACK=1 ${EXTRA_FLAGS}
-# Use the conda build in the mkl folder for BLAS
+cmake -LAH ../tools/python                              \
+  -DCMAKE_PREFIX_PATH="$PREFIX"                         \
+  -DCMAKE_BUILD_TYPE="Release"                          \
+  -DBoost_USE_STATIC_LIBS=0                             \
+  -DBoost_USE_STATIC_RUNTIME=0                          \
+  -DBOOST_ROOT="$PREFIX"                                \
+  -DBOOST_INCLUDEDIR="$PREFIX/include"                  \
+  -DBOOST_LIBRARYDIR="$PREFIX/lib"                      \
+  -DPYTHON_LIBRARY="$PYTHON_LIBRARY_PATH"               \
+  -DPYTHON_INCLUDE_DIR="$PREFIX/include/python$PY_STR"  \
+  -DPYTHON3=$PY3K                                       \
+  -DDLIB_PNG_SUPPORT=1                                  \
+  -DPNG_INCLUDE_DIR="$PREFIX/include"                   \
+  -DPNG_PNG_INCLUDE_DIR="$PREFIX/include"               \
+  -DPNG_LIBRARY="$PREFIX/lib/libpng.${DYNAMIC_EXT}"     \
+  -DZLIB_INCLUDE_DIRS="$PREFIX/include"                 \
+  -DZLIB_LIBRARIES="$PREFIX/lib/libz.${DYNAMIC_EXT}"    \
+  -DDLIB_JPEG_SUPPORT=1                                 \
+  -DJPEG_INCLUDE_DIR="$PREFIX/include"                  \
+  -DJPEG_LIBRARY="$PREFIX/lib/libjpeg.${DYNAMIC_EXT}"   \
+  -DDLIB_LINK_WITH_SQLITE3=1                            \
+  -DDLIB_NO_GUI_SUPPORT=1                               \
+  -DUSE_SSE2_INSTRUCTIONS=1                             \
+  -DUSE_SSE4_INSTRUCTIONS=$USE_SSE4                     \
+  -DUSE_AVX_INSTRUCTIONS=0                              \
+  -DDLIB_USE_BLAS=1                                     \
+  -DDLIB_USE_LAPACK=1                                   \
+  -DDLIB_USE_CUDA=0                                     \
+  -DDLIB_GIF_SUPPORT=0
 
-# On Travis, due to the heavy template expansion, we need
-# to limit the number of cores (and thus the memory
-# usage) else we get a compiler error.
-CMAKE_CPU_COUNT=1
-
-cmake --build . --config Release --target install -- -j${CMAKE_CPU_COUNT}
-
+make
+# Non-standard installation - copy manually
 cp dlib.so $SP_DIR
-
